901ac4fa10bc6c426b81691c771ea00d01d1a04b
Priyadarshee Kumar <priyadarsheekumar18@gmail.com>
2026-02-23 13:18:41 +0530

    Remove all folders except PythonAdvanceProgramming and PytestPOM

--- LabSessions/Day11_feb17/test_user.py
+++
@@ -1,2 +0,0 @@
-def test_user_creation(user):
-    assert user is not None
--- pyTest/Test_fixture_Session.py
+++
@@ -1,6 +0,0 @@
-import pytest
-@pytest.mark.usefixtures("sessionsetup")
-def test_one():
-    print("Testcase1")
-def test_two():
-    print("Testcase2")

caf63289eaef3cc33bce6d8cfb6888cf2b530f43
Priyadarshee Kumar <97228472+Kp-Cyber3721@users.noreply.github.com>
2026-02-25 18:36:56 +0530

    Add files via upload

---
+++ PytestPOM/Pages/login_page.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.NAME, "username")
+        self.password_input = (By.NAME, "password")
+        self.login_button = (By.XPATH, "//button[@type='submit']")
+        self.error_message = (By.CSS_SELECTOR, "div.oxd-alert-content.oxd-alert-content--error")
+
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).clear()
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).clear()
+        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
+
+    def get_error_message(self):
+        # Wait up to 5 seconds for the error message to appear
+        try:
+            msg = WebDriverWait(self.driver, 5).until(
+                EC.visibility_of_element_located(self.error_message)
+            )
+            return msg.text
+        except:
+            return ""
